3cfab2825aa80ff6763895ad73ffa0c3a154f3f8
Pranavan Pirahalathan <pranavan22@gmail.com>
2020-06-13 23:38:33 -0400

    Crawler added

---
+++ crawler.py
@@ -0,0 +1,20 @@
+import requests
+import csv
+import time
+from bs4 import BeautifulSoup
+
+url = "https://old.reddit.com/r/datascience/"
+# Headers to mimic a browser visit
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+# Returns a requests.models.Response object
+page = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(page.text, 'html.parser')
+domains = soup.find_all("span", class_="domain")
+
+for domain in domains:
+    if domain != "(self.datascience)":
+        continue
+
+    print(domain.text)
