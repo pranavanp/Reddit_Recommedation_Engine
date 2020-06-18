735881bf22a24fb3c1c01ed5196d5b9d8a465af9
Pranavan Pirahalathan <pranavan22@gmail.com>
2020-06-18 10:47:57 -0400

    Updates

--- SearchGenerator.py
+++ SearchGenerator.py
@@ -1,25 +1,8 @@
+import nltk
 import praw
 import json
-import Cleaner
+from Cleaner import Cleaner
 import yake
-
-def create_reddit_object(json_file="reddit_config.json", json_key="reddit_user_values"):
-    with open(json_file) as f:
-        data = json.load(f)
-        user_values = data[json_key]
-        reddit = praw.Reddit(client_id=user_values['client_id'],
-                             client_secret=user_values['client_secret'],
-                             user_agent=user_values['user_agent'],
-                             username=user_values['username'],
-                             password=user_values['password'])
-        return reddit
-
-
-reddit = create_reddit_object()
-
-url = "https://www.reddit.com/r/uwaterloo/comments/gomj1g/how_to_get_good_algorithms_and_data_structures/"
-submission = reddit.submission(url=url)
-
 
 class SearchGenerator:
     def __init__(self, url):
@@ -29,26 +12,49 @@ class SearchGenerator:
         with open(json_file) as f:
             data = json.load(f)
             user_values = data[json_key]
-            reddit = praw.Reddit(client_id=user_values['client_id'],
-                                 client_secret=user_values['client_secret'],
+            reddit = praw.Reddit(client_secret=user_values['client_secret'],
+                                 client_id=user_values['client_id'],
                                  user_agent=user_values['user_agent'],
                                  username=user_values['username'],
                                  password=user_values['password'])
             return reddit
 
-    def scrape_submission(self):
-        reddit = self.create_reddit()
-        self.submission = reddit.submission(url=url)
-        text = self.submission.selftext
+    def get_title(self):
+        return self.submission.title
+
+    def get_body(self):
+        return self.submission.selftext
+
+    def get_subreddit(self):
+        self.subreddit = self.submission.subreddit
+
+    def get_comments(self):
+        text = ""
         self.submission.comments.replace_more(limit=None)
         for comment in self.submission.comments.list():
             text += ' ' + comment.body
         return text
 
-    def get_cleantext(self,text):
+    def scrape_submission(self):
+        reddit = self.create_reddit()
+        self.submission = reddit.submission(url=url)
+        text = self.get_title()
+        text += ' ' + self.get_body()
+        if len(text) <= 1000:
+            text += ' ' + self.get_comments()
+        return text
+
+    def get_cleantext(self, text):
         cleaner = Cleaner()
         cleaned = cleaner.clean_text(text)
         return cleaned
 
-    def generate_keywords(self):
+    def extract_keywords(self):
+        content= self.get_cleantext(self.scrape_submission())
+        simple_kwextractor = yake.KeywordExtractor(content)
+        keywords = simple_kwextractor.extract_keywords(n=1)
+        return keywords
 
+url = "https://www.reddit.com/r/uwaterloo/comments/gomj1g/how_to_get_good_algorithms_and_data_structures/"
+run=SearchGenerator(url)
+print(run.extract_keywords())
