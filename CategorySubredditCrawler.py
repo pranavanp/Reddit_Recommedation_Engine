25df74084f428eb2a4f1c4f18969a3ff7f38d236
Pranavan Pirahalathan <pranavan22@gmail.com>
2020-06-18 07:16:52 -0400

    Refactored CategoryCrawler.py

--- CategorySubredditCrawler.py
+++ CategorySubredditCrawler.py
@@ -6,18 +6,18 @@ import concurrent.futures
 JSON_FILE='tag_url.json'
 #app = Celery('tasks', broker='amqp://localhost//')
 
+titles=[]
 def scraper(url):
     driver = webdriver.Firefox()
     driver.get(url)
-    title = driver.title
+    titles.append(driver.title)
     driver.close()
-    return
 
 with open(JSON_FILE) as f:
     categories = json.load(f)
     for category_key in categories:
         collection = categories[category_key]
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            res = executor.map(scraper(url))
-            print(res.result())
-        for (category,url) in collection.items():
+            res = executor.map(lambda category,url:scraper(url),collection.items())
+
+
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Reddit_Recommedation_Engine
+- Used Celery and RabbitMQ to asynchronously
+
+Tasks To Do:
+- Create a list of all the subreddits per category to a dataframe
+- Use the list of reddits to classify which category your current reddit falls under
+- If not part of the list what do we do??
+- Go through each subreddit part of that category giving priority to your own first and go through the top go through the posts and compare the title
+- If the titles yield similar accuracy then check out those article bodies and select top 10 recommendations of related articles
+- Of the ones that give the top score 
+- return the answer in text
+
+Future Goals:
+- Make it available as a Chrome Extension using Brython 
